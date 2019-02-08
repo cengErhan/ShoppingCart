9066d35f0fcdef84d405513885241ffcc5ea3002
Erhan Aslan <erhanjava@gmail.com>
2019-02-08 10:24:49 +0300

    V3

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
 
 
-
+//creating the mobile responsive menu
 function showMobilMenu() {
-    if(document.getElementById("responsive").className == "resnavmenu"){
-        document.getElementById("responsive").className = "block" ;
+    if (document.getElementById("responsive").className == "resnavmenu") {
+        document.getElementById("responsive").className = "block";
     } else {
         document.getElementById("responsive").className = "resnavmenu"
     }
 }
+
+//getting the li tags
+var elements = document.getElementById("responsive").firstElementChild.children;
+
+//new function
+function newFonk() {
+    console.log("newFonk is working");
+}
+
+//active class removing and adding new tag
+for (let index = 0; index < elements.length; index++) {
+    elements[index].onclick = function () {
+        for (let j = 0; j < elements.length; j++) {
+            elements[j].classList.remove("active");
+        }
+        elements[index].classList.add("active");
+    };
+}
+
+
 
